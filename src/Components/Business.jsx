0af61c8d88ff7224b8b05d9e2764674ee20fe08b
Jonathan Bruckman <jbruckman86@gmail.com>
2023-04-12 13:13:09 -0400

    completed above fold/stats

--- src/Components/Business.jsx
+++ src/Components/Business.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { features } from '../constants';
+import styles, { layout } from  '../style';
+import Button from './Button'
 
-const Business = () => {
-  return (
-    <div>Business</div>
+const Business = () => (
+    <div>
+
+    </div>
   )
-}
+
 
 export default Business
