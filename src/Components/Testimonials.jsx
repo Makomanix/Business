3fa3a030a2c8ee5b9d78b29290f81e4b54929627
Jonathan Bruckman <jbruckman86@gmail.com>
2023-04-17 12:26:25 -0400

    corrected typo Clients to clients

--- src/Components/Testimonials.jsx
+++ src/Components/Testimonials.jsx
@@ -4,7 +4,7 @@ import styles from '../style';
 import FeedbackCard from './FeedbackCard';
 
 const Testimonials = () => (
-    <section id='Clients' className={`${styles.paddingY} ${styles.flexCenter} flex-col relative`}>
+    <section id='clients' className={`${styles.paddingY} ${styles.flexCenter} flex-col relative`}>
       {/* TODO */}
       <div className='absolute z-[0] w-[60%] h-[60%] -right-[50%] rounded-full blue__gradient'/>
 
